835bdaf17e026d40a4f72f5669c2464de1d7fbd2
dimayefremov <efremov2293@gmail.com>
2021-11-08 01:43:48 +0200

    Set 'lighthouse.schema.register' config, tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,22 +4,32 @@ declare(strict_types=1);
 
 namespace DM\LighthouseSchemaGenerator\Tests;
 
-use DM\LighthouseSchemaGenerator\SchemaGeneratorServiceProvider;
+use Illuminate\Foundation\Application;
 use Orchestra\Testbench\TestCase as BaseTestCase;
+use DM\LighthouseSchemaGenerator\SchemaGeneratorServiceProvider;
 
 class TestCase extends BaseTestCase
 {
     /**
      * Get package providers.
      *
-     * @param  \Illuminate\Foundation\Application  $app
+     * @param  Application  $app
      *
      * @return array
      */
-    protected function getPackageProviders($app)
+    protected function getPackageProviders($app): array
     {
         return [
             SchemaGeneratorServiceProvider::class,
         ];
+    }
+
+    /**
+     * @param Application $app
+     */
+    protected function resolveApplicationConfiguration($app)
+    {
+        parent::resolveApplicationConfiguration($app);
+        $app['config']['lighthouse.schema.register'] = $app['path.config'] . '/graphql/schema.graphql';
     }
 }
