c2751218f720d3275400f0fee8d624a3d551226e
dimayefremov <efremov2293@gmail.com>
2021-11-11 02:35:32 +0200

    Remove constructor from command class

--- src/Commands/MakeGraphqlSchemaCommand.php
+++ src/Commands/MakeGraphqlSchemaCommand.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace DM\LighthouseSchemaGenerator\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Database\Eloquent\Model;
 use Safe\Exceptions\FilesystemException;
 use DM\LighthouseSchemaGenerator\Helpers\FileUtils;
 use DM\LighthouseSchemaGenerator\Parsers\ModelParser;
@@ -27,55 +28,37 @@ class MakeGraphqlSchemaCommand extends Command
      */
     protected $description = 'Lighthouse schema generator';
 
-    /** @var ModelsUtils */
-    private $modelsUtils;
-
-    /** @var FileUtils */
-    private $fileUtils;
-
-    /** @var ModelParser */
-    private $modelParser;
-
     /**
-     * Create a new command instance.
-     *
-     * @return void
+     * @param ModelsUtils $modelsUtils
+     * @param FileUtils $fileUtils
+     * @param ModelParser $modelParser
      */
-    public function __construct(ModelsUtils $modelsUtils, FileUtils $fileUtils, ModelParser $modelParser)
-    {
-        parent::__construct();
-
-        $this->modelsUtils = $modelsUtils;
-        $this->fileUtils = $fileUtils;
-        $this->modelParser = $modelParser;
-    }
-
-    public function handle(): void
+    public function handle(ModelsUtils $modelsUtils, FileUtils $fileUtils, ModelParser $modelParser): void
     {
         $modelsPath = $this->getModelsPathFromOption();
-        $path = $this->fileUtils->exists(app_path($modelsPath)) ? $modelsPath : false;
+        $path = $fileUtils->exists(app_path($modelsPath)) ? $modelsPath : false;
 
         if ($path !== false) {
-            $files = $this->fileUtils->getAllFiles($path);
-            $models = $this->modelsUtils->getModels($files, $path);
+            $files = $fileUtils->getAllFiles($path);
+            $models = $modelsUtils->getModels($files, $path);
             $schemaFolder = $this->getGraphqlSchemaPath();
 
             /** @var bool $force */
             $force = $this->option('force');
 
-            $models->each(function ($model) use ($schemaFolder, $force) {
-                $schemaFileName = $this->fileUtils->generateFileName(class_basename($model));
+            $models->each(function (Model $model) use ($schemaFolder, $force, $fileUtils, $modelParser) {
+                $schemaFileName = $fileUtils->generateFileName(class_basename($model));
                 $schemaPath = "{$schemaFolder}/{$schemaFileName}";
 
-                if (! $force && $this->fileUtils->exists($schemaPath)) {
+                if (! $force && $fileUtils->exists($schemaPath)) {
                     $question = "The {$schemaFileName} file exists. Do you want to rewrite file?";
                     if (! $this->confirm($question)) return true;
                 }
 
-                $content = $this->modelParser->parse($model);
+                $content = $modelParser->parse($model);
 
                 try {
-                    $schema = $this->fileUtils->filePutContents($schemaPath, $content);
+                    $schema = $fileUtils->filePutContents($schemaPath, $content);
                 } catch (FilesystemException $exception) {
                     $this->error($exception->getMessage());
                     return true;
